b5a08235b77dd8f8158b66846c4fcbdc4cec86e1
skmtrd <skmtgame23@gmail.com>
2024-05-23 18:00:57 +0900

    UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
   const [board, setBoard] = useState(create2DArray(20, 10, 0));
   const [isActive, setIsActive] = useState(false);
   const [seconds, setSeconds] = useState(0);
-  const [blockHistory, setBlockHitory] = useState<number[][]>([generateUniqueList(), [], [0]]);
+  const [blockHistory, setBlockHitory] = useState<number[][]>([[], [], []]);
   const nextBlockBoard = create2DArray(4, 4, 0);
   const blockMove = [0, 0, 0, 0, 0, 0];
   const removeLine = board.length - 20;
@@ -25,8 +25,24 @@ const Home = () => {
     setBoard(create2DArray(20, 10, 0));
     setIsActive(true);
   };
+
   const stopTimer = () => {
     setIsActive(false);
+  };
+
+  const toggleButtonHandler = () => {
+    if (isActive) {
+      setIsActive(false);
+    } else {
+      setIsActive(true);
+    }
+  };
+
+  const resetButtonHandler = () => {
+    setBoard(create2DArray(20, 10, 0));
+    setSeconds(0);
+    setIsActive(false);
+    setBlockHitory([generateUniqueList(), [], [0]]);
   };
 
   const newBlockMove = structuredClone(blockMove);
@@ -49,6 +65,12 @@ const Home = () => {
       case 4:
         newBlockMove[3] = 1;
         break;
+      case 5:
+        resetButtonHandler();
+        break;
+      case 6:
+        toggleButtonHandler();
+        break;
     }
   };
 
@@ -65,6 +87,10 @@ const Home = () => {
       newBlockMove[3] = 1;
     } else if (key === 'ArrowUp') {
       newBlockMove[4] = 1;
+    } else if (key === 'Enter') {
+      isActive ? stopTimer() : startTimer();
+    } else if (key === 'r' || key === 'R') {
+      resetButtonHandler();
     }
   };
   if (newBlockHistory[0].length === 0) {
@@ -73,11 +99,12 @@ const Home = () => {
       newBlockHistory[0].push(blocks);
     }
   }
-  if (isActive) {
+  if (board.flat().filter((cell) => cell === 1 || cell === 2).length !== 0) {
     for (const position of blocks[newBlockHistory[0][0]]) {
       nextBlockBoard[position[0] + 1][position[1] - 3] = 1;
     }
   }
+
   useEffect(() => {
     let interval = null;
     if (isActive) {
@@ -147,18 +174,14 @@ const Home = () => {
 
   return (
     <div className={styles.container} onKeyDown={keyDownHandler} tabIndex={0}>
-      <div
-        className={styles.stratButton}
-        onClick={startTimer}
-        style={{ visibility: isActive ? 'hidden' : 'visible' }}
-      >
-        Start
-      </div>
-      <div className={styles.topInformation}>
-        <div>Level : {Math.floor(removeLine / 10 + 1)}</div>
-        <div style={{ marginLeft: 100 }}>remove : {removeLine}</div>
-      </div>
       <div className={styles.base}>
+        <div className={styles.topInformation}>
+          <div>Level : {Math.floor(removeLine / 10 + 1)}</div>
+          <div style={{ marginLeft: 100 }}>remove : {removeLine}</div>
+        </div>
+        <div className={styles.curtain} style={{ visibility: isActive ? 'hidden' : 'visible' }}>
+          Press Start or Enter Key
+        </div>
         <div className={styles.board}>
           {board.map((row, y) =>
             row.map((cell, x) => (
@@ -175,7 +198,6 @@ const Home = () => {
           )}
         </div>
         <div className={styles.informationBoard}>
-          <div>NextBlock</div>
           <div className={styles.informationBoardBox}>
             {nextBlockBoard.map((row, y) =>
               row.map((cell, x) => (
@@ -185,9 +207,9 @@ const Home = () => {
                   style={{
                     backgroundColor: nextBlockBoard[y][x] === 0 ? '#98d7f5' : '#a556ff',
                     borderColor:
-                      nextBlockBoard[y][x] === 0 ? '#beefff' : ' #cecece  #c69bff  #c69bff #cecece',
+                      nextBlockBoard[y][x] === 0 ? '#98d7f5' : ' #cecece  #c69bff  #c69bff #cecece',
                     borderWidth: nextBlockBoard[y][x] === 0 ? 1 : 3,
-                    borderRadius: '4px',
+                    borderRadius: 5,
                     width: '20px',
                     height: '20px',
                   }}
@@ -195,31 +217,48 @@ const Home = () => {
               )),
             )}
           </div>
-          <div className={styles.buttonsBox}>
-            <div className={styles.buttons} />
-            <div className={styles.buttons} />
+          <div className={styles.buttonsBox} style={{ gap: 2, marginTop: 80 }}>
+            <div className={styles.buttons} onClick={() => touchControlHandler(0)}>
+              ↻
+            </div>
           </div>
-          {/* <div className={styles.controlPanel}>
-            <div className={styles.panelCell} />
-            <div className={styles.panelCell} onClick={() => touchControlHandler(0)}>
-              ⟳
+          <div className={styles.buttonsBox} style={{ gap: 2, marginTop: 13 }}>
+            <div className={styles.buttons} onClick={() => touchControlHandler(1)}>
+              ←
             </div>
-            <div className={styles.panelCell} />
-            <div className={styles.panelCell} onClick={() => touchControlHandler(1)}>
-              ←{' '}
+            <div className={styles.buttons} onClick={() => touchControlHandler(2)}>
+              ↓
             </div>
-            <div className={styles.panelCell} onClick={() => touchControlHandler(2)}>
-              ↓{' '}
+            <div className={styles.buttons} onClick={() => touchControlHandler(3)}>
+              →
             </div>
-            <div className={styles.panelCell} onClick={() => touchControlHandler(3)}>
-              →{' '}
+          </div>
+          <div
+            className={styles.buttons}
+            style={{ marginTop: 13 }}
+            onClick={() => touchControlHandler(4)}
+          >
+            ↓↓↓
+          </div>
+          <div
+            className={styles.buttonsBox}
+            style={{ marginTop: 130, flexFlow: 'column', gap: 15 }}
+          >
+            <div
+              className={styles.buttons}
+              onClick={() => touchControlHandler(5)}
+              style={{ width: 100 }}
+            >
+              Restart
             </div>
-            <div className={styles.panelCell} />
-            <div className={styles.panelCell} onClick={() => touchControlHandler(4)}>
-              ↓↓↓
+            <div
+              className={styles.buttons}
+              onClick={() => touchControlHandler(6)}
+              style={{ width: 100 }}
+            >
+              {isActive ? 'Stop' : 'Start'}
             </div>
-            <div className={styles.panelCell} />
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
